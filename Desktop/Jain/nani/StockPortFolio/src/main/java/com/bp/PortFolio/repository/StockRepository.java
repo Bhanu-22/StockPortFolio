3f07e9d1e0cd362073850d35d14edb037eab7af7
Bhanu Prakash C M <serpentssouthside0@gmail.com>
2025-04-24 23:58:19 +0530

    Delete Desktop/Jain/nani/StockPortFolio directory

--- Desktop/Jain/nani/StockPortFolio/src/main/java/com/bp/PortFolio/repository/StockRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bp.PortFolio.repository;
-
-import com.bp.PortFolio.model.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockRepository extends JpaRepository<Stock, Long> {
-}
